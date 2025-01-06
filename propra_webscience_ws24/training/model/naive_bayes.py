8361d1e86379fe4e2b123e6fa3dbc2f37804c752
Felix Lindner <felix.lindner@posteo.de>
2025-01-06 18:36:34 +0100

    use BernoulliNB as model for NaiveBayes Wrapper

--- propra_webscience_ws24/training/model/naive_bayes.py
+++ propra_webscience_ws24/training/model/naive_bayes.py
@@ -1,12 +1,12 @@
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import BernoulliNB
 
 from propra_webscience_ws24.training.model.model_base import ModelBase, ModelType
 
 
-class NaiveBayes(ModelBase[GaussianNB]):
+class NaiveBayes(ModelBase[BernoulliNB]):
 
-    def _get_model(self, model_args) -> GaussianNB:
-        return GaussianNB(**model_args)
+    def _get_model(self, model_args) -> BernoulliNB:
+        return BernoulliNB(**model_args)
 
     def _get_model_type(self) -> ModelType:
         return ModelType.NAIVE_BAYES
