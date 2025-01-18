fc9ff6bc28c3e1b60313f91315ce9690ef0bb7db
Felix Lindner <felix.lindner@posteo.de>
2025-01-18 12:42:51 +0100

    fixed some runtime errors

--- propra_webscience_ws24/training/finetuning.py
+++ propra_webscience_ws24/training/finetuning.py
@@ -1,63 +1,74 @@
 import torch
 import numpy as np
-from datasets import load_dataset
+from datasets import load_dataset  # type: ignore[attr-defined]
 from evaluate import load
 from transformers import AutoTokenizer
 from transformers import DataCollatorWithPadding
 from transformers import AutoModelForSequenceClassification
 from transformers import TrainingArguments, Trainer
 
-print(f'cuda enabled: {torch.cuda.is_available()}')
+print(f"cuda enabled: {torch.cuda.is_available()}")
 
 sentiment140 = load_dataset("sentiment140", trust_remote_code=True)
 
-print(sentiment140['train'].info)
+print(sentiment140["train"].info)
 
-small_train_dataset = sentiment140["train"].shuffle(seed=42).select([i for i in list(range(10000))])
+small_train_dataset = (
+    sentiment140["train"].shuffle(seed=42).select([i for i in list(range(10000))])
+)
 small_test_dataset = sentiment140["test"]
+
+small_train_dataset = small_train_dataset.rename_column("sentiment", "label")
+small_test_dataset = small_test_dataset.rename_column("sentiment", "label")
 
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
+
 def preprocess_function(examples):
-   return tokenizer(examples["text"], truncation=True)
+    return tokenizer(examples["text"], truncation=True)
+
 
 tokenized_train = small_train_dataset.map(preprocess_function, batched=True)
 tokenized_test = small_test_dataset.map(preprocess_function, batched=True)
 
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=2)
+model = AutoModelForSequenceClassification.from_pretrained(
+    "distilbert-base-uncased", num_labels=2
+)
 
 
 def compute_metrics(eval_pred):
-   load_accuracy = load("accuracy")
-   load_f1 = load("f1")
+    load_accuracy = load("accuracy")
+    load_f1 = load("f1")
 
-   logits, labels = eval_pred
-   predictions = np.argmax(logits, axis=-1)
-   accuracy = load_accuracy.compute(predictions=predictions, references=labels)["accuracy"]
-   f1 = load_f1.compute(predictions=predictions, references=labels)["f1"]
-   return {"accuracy": accuracy, "f1": f1}
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    accuracy = load_accuracy.compute(predictions=predictions, references=labels)[
+        "accuracy"
+    ]
+    f1 = load_f1.compute(predictions=predictions, references=labels)["f1"]
+    return {"accuracy": accuracy, "f1": f1}
+
 
 training_args = TrainingArguments(
-   output_dir="output/",
-   learning_rate=2e-5,
-   per_device_train_batch_size=16,
-   per_device_eval_batch_size=16,
-   num_train_epochs=2,
-   weight_decay=0.01,
-   save_strategy="epoch",
-   push_to_hub=True,
+    output_dir="output/",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=2,
+    weight_decay=0.01,
+    save_strategy="epoch",
 )
 
 trainer = Trainer(
-   model=model,
-   args=training_args,
-   train_dataset=tokenized_train,
-   eval_dataset=tokenized_test,
-   tokenizer=tokenizer,
-   data_collator=data_collator,
-   compute_metrics=compute_metrics,
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_train,
+    eval_dataset=tokenized_test,
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
 )
 
 trainer.train()
