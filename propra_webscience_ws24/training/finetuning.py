c7db267706be4d0c55969f7078ea814ef018e148
Felix Lindner <felix.lindner@posteo.de>
2025-01-23 21:11:11 +0100

    train distilbert and roberta with different params

--- propra_webscience_ws24/training/finetuning.py
+++ propra_webscience_ws24/training/finetuning.py
@@ -7,70 +7,6 @@ from transformers import DataCollatorWithPadding
 from transformers import AutoModelForSequenceClassification
 from transformers import TrainingArguments, Trainer
 
-model_name = "cardiffnlp/twitter-roberta-base-sentiment"
-
-print(f"cuda enabled: {torch.cuda.is_available()}")
-
-sentiment140 = load_dataset("sentiment140", trust_remote_code=True)
-
-dataset_size = 10000
-
-positive = (
-    sentiment140["train"]
-    .select([i for i in list(range(0, 799999))])
-    .shuffle(seed=42)
-    .select([i for i in list(range(int(dataset_size / 2)))])
-)
-negative = (
-    sentiment140["train"]
-    .select([i for i in list(range(800000, 1599999))])
-    .shuffle(seed=42)
-    .select([i for i in list(range(int(dataset_size / 2)))])
-)
-
-small_train_dataset = combine.concatenate_datasets([positive, negative])
-
-
-#
-# small_train_dataset = (
-#     sentiment140["train"].shuffle(seed=42).select([i for i in list(range(10000))])
-# )
-small_test_dataset = sentiment140["test"]
-
-small_train_dataset = small_train_dataset.rename_column("sentiment", "label")
-small_test_dataset = small_test_dataset.rename_column("sentiment", "label")
-
-sentiment_map = {
-    0: 0,
-    2: 1,
-    4: 2,
-}
-
-
-def map_sentiment(example):
-    example["label"] = sentiment_map[example["label"]]
-    return example
-
-
-small_train_dataset = small_train_dataset.map(map_sentiment)
-small_test_dataset = small_test_dataset.map(map_sentiment)
-
-small_test_dataset = small_test_dataset.filter(lambda example: example["label"] != 1)
-
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-
-def preprocess_function(examples):
-    return tokenizer(examples["text"], truncation=True)
-
-
-tokenized_train = small_train_dataset.map(preprocess_function, batched=True)
-tokenized_test = small_test_dataset.map(preprocess_function, batched=True)
-
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-
 
 def compute_metrics(eval_pred):
     load_accuracy = load("accuracy")
@@ -83,28 +19,161 @@ def compute_metrics(eval_pred):
     return {"accuracy": accuracy, "f1": f1}
 
 
-training_args = TrainingArguments(
-    output_dir="output/",
-    learning_rate=1e-5,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    num_train_epochs=2,
-    save_strategy="epoch",
-)
-
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_train,
-    eval_dataset=tokenized_test,
-    processing_class=tokenizer,
-    data_collator=data_collator,
-    compute_metrics=compute_metrics,
-)
+def preprocess_function(examples, tokenizer):
+    return tokenizer(examples["text"], truncation=True)
 
 
-print(trainer.evaluate())
+def map_sentiment(example, mapper):
+    example["label"] = mapper[example["label"]]
+    return example
 
-trainer.train()
 
-print(trainer.evaluate())
+def train_parans(
+    train_dataset,
+    test_dataset,
+    model_name,
+    dataset_size,
+    learning_rate,
+    tokenizer,
+    mapper,
+):
+    positive = (
+        train_dataset.select([i for i in list(range(0, 799999))])
+        .shuffle(seed=42)
+        .select([i for i in list(range(int(dataset_size / 2)))])
+    )
+    negative = (
+        train_dataset.select([i for i in list(range(800000, 1599999))])
+        .shuffle(seed=42)
+        .select([i for i in list(range(int(dataset_size / 2)))])
+    )
+    small_train_dataset = combine.concatenate_datasets([positive, negative])
+
+    small_train_dataset = small_train_dataset.map(
+        lambda example: map_sentiment(example, mapper)
+    )
+
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+    tokenized_train = small_train_dataset.map(
+        lambda example: preprocess_function(example, tokenizer), batched=True
+    )
+
+    training_args = TrainingArguments(
+        output_dir="output/",
+        learning_rate=learning_rate,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=2,
+        save_strategy="epoch",
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_train,
+        eval_dataset=test_dataset,
+        processing_class=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+    )
+
+    trainer.train()
+
+    print(trainer.evaluate())
+
+
+def train_model(model_name, dataset, mapper, dataset_sizes, learning_rates):
+    train_dataset = dataset["train"]
+    test_dataset = dataset["test"]
+
+    train_dataset = train_dataset.rename_column("sentiment", "label")
+    test_dataset = test_dataset.rename_column("sentiment", "label")
+
+    small_train_dataset = train_dataset.shuffle(seed=42).select(
+        [i for i in list(range(1000))]
+    )
+
+    test_dataset = test_dataset.filter(lambda example: example["label"] != 2)
+
+    test_dataset = test_dataset.map(lambda example: map_sentiment(example, mapper))
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+
+    tokenized_train = small_train_dataset.map(
+        lambda example: preprocess_function(example, tokenizer), batched=True
+    )
+    tokenized_test = test_dataset.map(
+        lambda example: preprocess_function(example, tokenizer), batched=True
+    )
+
+    training_args = TrainingArguments(
+        output_dir="output/",
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_train,
+        eval_dataset=tokenized_test,
+        processing_class=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+    )
+    print(trainer.evaluate())
+    for dataset_size in dataset_sizes:
+        for learning_rate in learning_rates:
+            print(f"learning rate: {learning_rate}, dataset size: {dataset_size}")
+            train_parans(
+                train_dataset,
+                tokenized_test,
+                model_name,
+                dataset_size,
+                learning_rate,
+                tokenizer,
+                mapper,
+            )
+
+
+print(f"cuda enabled: {torch.cuda.is_available()}")
+
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+distilbert = "distilbert-base-uncased"
+
+sentiment140 = load_dataset("sentiment140", trust_remote_code=True)
+
+sentiment_map_roberta = {
+    0: 0,
+    4: 2,
+}
+
+sentiment_map_distilbert = {
+    0: 0,
+    4: 1,
+}
+
+model_names = [distilbert, roberta]
+
+sentiment_maps = {
+    roberta: sentiment_map_roberta,
+    distilbert: sentiment_map_distilbert,
+}
+
+learning_rates = [1e-3, 1e-4, 1e-5, 1e-6]
+
+dataset_sizes = [2500, 5000, 7500, 10000]
+
+for model_name in model_names:
+    train_model(
+        model_name,
+        sentiment140,
+        sentiment_maps[model_name],
+        dataset_sizes,
+        learning_rates,
+    )
