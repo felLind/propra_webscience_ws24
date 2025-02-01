52337488352e655bf9ca499d11879a786283adbc
AndiFr <franke.and@gmail.com>
2025-02-01 12:06:08 +0100

    feat: add classifier on top of _DeepSeek R1_ distilled models

---
+++ propra_webscience_ws24/training/llm/finetuning_deepseek.py
@@ -0,0 +1,167 @@
+import dataclasses
+import statistics
+import time
+
+import pandas as pd
+import torch
+from loguru import logger
+from torch import nn
+from transformers import (
+    AutoModelForSequenceClassification,
+    DataCollatorWithPadding,
+    TrainingArguments,
+    Trainer,
+)
+from transformers import AutoTokenizer
+
+from propra_webscience_ws24 import constants
+from propra_webscience_ws24.training.llm.data_splits import (
+    get_train_and_test_datasets,
+    create_train_and_eval_split,
+)
+from propra_webscience_ws24.training.llm.utils import tokenize_text, compute_metrics
+
+SEED = 42
+# DEEPSEEK_MODEL_NAME = 'deepseek-ai/DeepSeek-R1-Distill-Llama-8B'
+DEEPSEEK_MODEL_NAME = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
+
+SENTIMENT_MAPPING = {
+    0: 0,
+    4: 1,
+}
+
+LEARNING_RATES = [1e-4]
+
+DATASET_SIZES = [1_000]
+
+logger.info(f"cuda enabled: {torch.cuda.is_available()}")
+
+
+def map_sentiment(example, mapper):
+    example["label"] = mapper[example["label"]]
+    return example
+
+
+@dataclasses.dataclass
+class EvalResult:
+    model_name: str
+    data_size: int | None
+    learning_rate: float | None
+    accuracy: float
+    f1: float
+
+
+def create_deepseek_classifier_model(model_name, output_dim=1536, n_labels=2):
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+
+    class CustomHead(nn.Module):
+        def __init__(self, output_dim, n_labels):
+            super().__init__()
+            self.dense = nn.Linear(
+                in_features=output_dim, out_features=output_dim, bias=True
+            )
+            self.dropout = nn.Dropout(p=0.1)
+            self.out_proj = nn.Linear(
+                in_features=output_dim, out_features=n_labels, bias=False
+            )
+
+        def forward(self, x):
+            x = self.dense(x)
+            x = torch.relu(x)  # Todo: Modify?
+            x = self.dropout(x)
+            x = self.out_proj(x)
+            return x
+
+    # Replace the model's classifier (last layer)
+    model.score = CustomHead(output_dim, n_labels)
+
+    logger.debug(f"{model=}")
+
+    return model
+
+
+def train_classifier_and_evaluate(ds_train, ds_eval, ds_test, learning_rate):
+    model = create_deepseek_classifier_model(DEEPSEEK_MODEL_NAME)
+    model.config.pad_token_id = model.config.eos_token_id
+    tokenizer = AutoTokenizer.from_pretrained(DEEPSEEK_MODEL_NAME)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    tokenized_train = ds_train.map(
+        lambda example: tokenize_text(example, tokenizer), batched=True
+    )
+    tokenized_eval = ds_eval.map(
+        lambda example: tokenize_text(example, tokenizer), batched=True
+    )
+    tokenized_test = ds_test.map(
+        lambda example: tokenize_text(example, tokenizer), batched=True
+    )
+
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    training_args = TrainingArguments(
+        output_dir="output/",
+        learning_rate=learning_rate,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=4,
+        save_strategy="no",
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_train,
+        eval_dataset=tokenized_eval,
+        processing_class=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+    )
+
+    logger.info(f"Fine Tuning Model: {dataset_size=}, {learning_rate=}")
+    trainer.train()
+
+    logger.info("Evaluating Model with test data...")
+    result = trainer.evaluate(tokenized_test)
+
+    eval_result = EvalResult(
+        model_name=DEEPSEEK_MODEL_NAME,
+        data_size=len(ds_train) + len(ds_eval),
+        learning_rate=learning_rate,
+        accuracy=result["eval_accuracy"]["accuracy"],
+        f1=statistics.mean(result["eval_f1"]["f1"]),
+    )
+    logger.info(f"Fine Tuned Model:\n{eval_result}")
+    return eval_result
+
+
+if __name__ == "__main__":
+    logger.info("fine tuning started")
+
+    eval_results = []
+
+    for dataset_size in DATASET_SIZES:
+        ds_train_, ds_test = get_train_and_test_datasets(SENTIMENT_MAPPING)
+
+        ds_train, ds_eval = create_train_and_eval_split(
+            ds_train_, SENTIMENT_MAPPING[4], dataset_size=dataset_size
+        )
+        for learning_rate in LEARNING_RATES:
+            result = train_classifier_and_evaluate(
+                ds_train,
+                ds_eval,
+                ds_test,
+                learning_rate,
+            )
+            eval_results.append(result)
+
+    logger.info("fine tuning done")
+
+    for eval_result in sorted(eval_results, key=lambda x: (x.model_name, x.accuracy)):
+        logger.info(eval_result)
+
+    df = pd.DataFrame(dataclasses.asdict(eval_result) for eval_result in eval_results)
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+
+    df.to_csv(
+        constants.RESULTS_PATH / f"fine_tuning_deepseek_{timestr}.csv", index=False
+    )
